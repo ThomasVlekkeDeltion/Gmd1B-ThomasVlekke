bf7be43ee29a288911f2abf9866712346594bcbf
ThomasVlekkeDeltion <97042453@st.deltion.nl>
2016-11-04 20:40:48 +0100

    extra uitleg bij één script

--- P1/Assets/Classes/LivesText.cs
+++ P1/Assets/Classes/LivesText.cs
@@ -8,7 +8,10 @@ public class LivesText : MonoBehaviour {
 
     public void ChangeScore(int points)
     {
+        //Hier pak ik de int van het script lives en zeg ik dat lives = lives + points (points = 3)
         GameObject.Find("Manager").GetComponent<Lives>().lives += points;
+        //Hier verander ik "lives" naar een string
         livestext.text = GameObject.Find("Manager").GetComponent<Lives>().lives.ToString();
+
     }
 }
